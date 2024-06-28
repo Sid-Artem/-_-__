da754817dac0b14f599d6b86c50b6dd6391786eb
Сидоров Артем <sidoroffartemii@gmail.com>
2024-06-28 21:48:05 +0300

    Добавьте файлы проекта.

---
+++ Задача 1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    internal class Program
+    {
+        static double GetSqrt(double a, double b, double c)
+        {
+            double p= (a+b+c)/2;
+            return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите стороны первого треугольника");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double z1 = double.Parse(Console.ReadLine());
+            double v1 = GetSqrt(x1, y1,z1);
+
+            Console.WriteLine("Введите стороны второго треугольника");
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double z2 = double.Parse(Console.ReadLine());
+            double v2 = GetSqrt(x2, y2, z2);
+
+            if (v1 > v2) Console.WriteLine("Площадь первого треугольника больше");
+            else
+                if (v1 < v2) Console.WriteLine("Площадь второго треугольника больше");
+            else Console.WriteLine("Площадь треугольников равна");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_2
+{
+    internal class Program
+    {   
+        static void GetCube(double a, out double s, out double v)
+        {
+            s = 6*(a*a);
+            v = a*a*a;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба");
+            double r= double.Parse(Console.ReadLine());
+            double s;
+            double v;
+            GetCube(r, out s, out v);
+            Console.WriteLine("{0:.00} {1:.00}" , s , v );
+            Console.ReadKey();
+        }
+    }
+}
